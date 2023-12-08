0b22a2359b7c07b6dd27498d7d534ffd17393189
Ionathan sideras <iona8ansideras@gmail.com>
2023-12-08 23:46:03 +0200

    Add functionality to add favorites to the database

--- src/helpers/addFavorites.js
+++ src/helpers/addFavorites.js
@@ -7,16 +7,23 @@ import {
   updateDoc,
 } from "firebase/firestore";
 
+// async function to add a favorite to the database
 export async function addFavorites(symbol) {
+  // Get the current user
   const user = auth?.currentUser;
 
+  // If user is logged in
   if (user?.uid) {
+    // Get the users collection from the database
     const usersCollection = collection(db, "users");
     const q = query(usersCollection, where("id", "==", user?.uid));
+    // Get the user document
     const querySnapshot = await getDocs(q);
 
+    // If the user document exists, update the favorites field
     if (!querySnapshot.empty) {
       const userDoc = querySnapshot.docs[0];
+      // Update the favorites field
       await updateDoc(userDoc.ref, {
         favorites: symbol,
       });
