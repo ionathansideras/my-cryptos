cfad45445f417eec40fc6e450a936084300d7d32
Ionathan sideras <iona8ansideras@gmail.com>
2023-12-17 19:55:28 +0200

    Add functions to handle theme color in local storage

--- src/helpers/handleLocalStorage.js
+++ src/helpers/handleLocalStorage.js
@@ -1,7 +1,11 @@
+// Function to retrieve the theme color from local storage
 export function getThemeColor() {
+  // Returns the value of the item in local storage with the key "theme"
   return localStorage.getItem("theme");
 }
 
+// Function to set the theme color in local storage
 export function setThemeColor(theme) {
+  // Sets the value of the item in local storage with the key "theme" to the provided theme
   return localStorage.setItem("theme", theme);
 }
