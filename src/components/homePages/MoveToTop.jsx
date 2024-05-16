48a0d18c0194f0815d886feafe3da5d6ac77a771
Ionathan Sideras <iona8ansideras@gmail.com>
2024-05-16 20:22:40 +0300

    chore: Update MoveToTop component with new images and fix indentation

--- src/components/homePages/MoveToTop.jsx
+++ src/components/homePages/MoveToTop.jsx
@@ -1,40 +1,40 @@
 import { moveUp } from "../../helpers/moveUp";
 import { useState, useEffect } from "react";
-import up3Img from "../../assets/up3.png";
-import up2Img from "../../assets/up2.png";
+import up3Img from "../../assets/up3.svg";
+import up2Img from "../../assets/up1.svg";
 // Import necessary modules from Redux toolkit
 import { useSelector } from "react-redux";
 
 export default function MoveToTop() {
-  const [showButton, setShowButton] = useState(false);
+    const [showButton, setShowButton] = useState(false);
 
-  // Redux state hook for theme
-  const theme = useSelector((store) => store.theme);
+    // Redux state hook for theme
+    const theme = useSelector((store) => store.theme);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setShowButton(window.pageYOffset > 300);
-    };
+    useEffect(() => {
+        const handleScroll = () => {
+            setShowButton(window.pageYOffset > 300);
+        };
 
-    window.addEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", handleScroll);
 
-    // Cleanup function to remove the event listener
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+        // Cleanup function to remove the event listener
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
-  return (
-    <button
-      style={{ display: showButton ? "inline-block" : "none" }}
-      onClick={moveUp}
-      className="moveUp-button"
-    >
-      {theme === "dark" ? (
-        <img src={up2Img} alt="up" />
-      ) : (
-        <img src={up3Img} alt="up" />
-      )}
-    </button>
-  );
+    return (
+        <button
+            style={{ display: showButton ? "inline-block" : "none" }}
+            onClick={moveUp}
+            className="moveUp-button"
+        >
+            {theme === "dark" ? (
+                <img src={up2Img} alt="up" />
+            ) : (
+                <img src={up3Img} alt="up" />
+            )}
+        </button>
+    );
 }
